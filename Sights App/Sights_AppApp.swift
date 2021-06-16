41b1914069ca45ea8aeb65b06e2891abd521a124
Justin Young <justinyoung371@yahoo.com>
2021-06-16 16:14:05 -0700

    - set up folders for views, models, and view models - rename default content view to launch view

--- Sights App/Sights_AppApp.swift
+++ Sights App/Sights_AppApp.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct Sights_AppApp: App {
+struct Sights_App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LaunchView()
         }
     }
 }
