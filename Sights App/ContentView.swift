f8e245b5fda8e510f26bb82896be487a37d8d67b
Justin Young <justinyoung371@yahoo.com>
2021-06-16 16:08:12 -0700

    Initial Commit

---
+++ Sights App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sights App
+//
+//  Created by Justin Young on 6/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sights App/Sights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sights_AppApp.swift
+//  Sights App
+//
+//  Created by Justin Young on 6/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Sights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
